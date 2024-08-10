5bfdfe206bd8282ba97b0b6b9cf7010cb57f5c41
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-08-10 09:26:06 +0500

    add main

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Main from './components/main/main'
 import Navbar from './components/navbar/navbar'
 
 function App(): JSX.Element {
 	return (
 		<>
 			<Navbar />
+			<Main />
 		</>
 	)
 }
---
+++ src/ui/banner/main_banner.tsx
@@ -0,0 +1,9 @@
+function Main_banner() {
+	return (
+		<>
+			<img src='/assets/main_banner.png' alt='404' className='w-full' />
+		</>
+	)
+}
+
+export default Main_banner
