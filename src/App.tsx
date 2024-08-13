aa9ebc6c60f416bf89e2ba3f153fa88302d1bf80
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-08-13 08:44:32 +0500

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import Main from './main-route/main'
-import Navbar from './components/navbar/navbar'
-import Wrapper from './layout/wrapper'
-import Main_banner from './ui/banner/main_banner'
-import Footer from './components/footer/footer'
+import Main from "./main-route/main";
+import Navbar from "./components/navbar/navbar";
+import Wrapper from "./layout/wrapper";
+import Main_banner from "./ui/banner/main_banner";
 
 function App(): JSX.Element {
-	return (
-		<>
-			<Navbar />
+  return (
+    <>
+      <Navbar />
 
-			<Wrapper>
-				<div className='sm:mt-[32px] sm:mx-[20px] xl:mx-0'>
-					<Main_banner />
-				</div>
-				<Main />
-			</Wrapper>
-      
-			<Footer />
-		</>
-	)
+      <Wrapper>
+        <div className="sm:mt-[32px] sm:mx-[20px] xl:mx-0">
+          <Main_banner />
+        </div>
+        <Main />
+      </Wrapper>
+    </>
+  );
 }
 
-export default App
+export default App;
