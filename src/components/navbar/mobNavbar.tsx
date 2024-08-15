67e8430cdcc7f955b77d42b92afe0e06c317ec7f
Forestest <hosting0web258@gmail.com>
2024-08-15 21:13:37 +0500

    EditNavbar

--- src/components/navbar/mobNavbar.tsx
+++ src/components/navbar/mobNavbar.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
+import { useModalContextNew } from '../../context/modalContext';
 
 interface mobileNavProps {
 	closeNav: () => void
 }
 
 const MobNavbar: React.FC<mobileNavProps> = ({ closeNav }): JSX.Element => {
+
+	const [city , setCity] = useState('')
+
+	const { openNavbarModal } = useModalContextNew();
+
+	useEffect(() => {
+		const storedData = localStorage.getItem('cityGorod');
+		if (storedData) {
+		  setCity(storedData);
+		}
+	  }, []);
+
 	return (
 		<motion.div
 			initial={{ x: '100%' }}
 			animate={{ x: 0 }}
 			exit={{ x: '100%' }}
+			className='h-[100vh] overflow-y-auto overscroll-contain'
 			transition={{ type: 'spring', stiffness: 300, damping: 30 }}
 		>
 			<div className='flex items-end justify-end'>
@@ -26,10 +41,18 @@ const MobNavbar: React.FC<mobileNavProps> = ({ closeNav }): JSX.Element => {
 					className='border-t border-b p-3 mt-[24px]'
 					style={{ borderColor: 'rgba(255, 255, 255, 0.30)' }}
 				>
-					<div className='flex justify-between items-center '>
+					<div className='flex justify-between items-center ' onClick={()=> 
+						{
+							closeNav();
+							openNavbarModal()
+						}
+					}>
 						<div className='flex items-center gap-[12px] text-[white]'>
 							<img src='/assets/map.png' alt='map' />
-							<p>Набереженые Челны</p>
+							{
+								city ? (<p>{city}</p>) : (<p>Выбрать город</p>)
+							}
+							
 						</div>
 						<div>
 							<img src='/assets/chevron-right.png' alt='chevron-right' />
