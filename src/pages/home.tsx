68f08f2e2035287b299bf89ef4182590d262c10e
Forestest <hosting0web258@gmail.com>
2024-08-15 10:05:03 +0500

    New city

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,16 @@
+"use client";
+import { useState } from "react";
 import Home_bez_gorod from "./home_bez_gorod";
+import Home_gorod from "./home_gorod";
 
 function Home(): JSX.Element {
-  return <div className="main-home-page mt-8 mx-4 sm:mx-[20px] xl:mx-0">
-    <Home_bez_gorod/>
-  </div>;
+  const [city, setCity] = useState("Moskva");
+
+  return (
+    <div className="main-home-page mt-8 mx-4 sm:mx-[20px] xl:mx-0">
+      {!city ? <Home_bez_gorod /> : <Home_gorod city={city} />}
+    </div>
+  );
 }
 
 export default Home;
