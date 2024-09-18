24a8289aed03768b3f97aa4ebeccc7b03307d744
Forestest <hosting0web258@gmail.com>
2024-09-18 22:34:16 +0500

    Likeee

--- src/pages/liked.tsx
+++ src/pages/liked.tsx
@@ -1,4 +1,4 @@
-import { Box, CloseButton } from "@chakra-ui/react";
+import { Box, CloseButton, SimpleGrid } from "@chakra-ui/react";
 import data from "../data/home_bez_gorod_data";
 import CardUI from "../ui/card/card";
 
@@ -9,17 +9,16 @@ export default function Liked() {
         <h2 className="text-[30px] hidden sm:block font-OpenSans font-semibold text-[#3B4255]">Избранное</h2>
 
         <div className="cards-list mt-6 sm:mt-8 flex flex-row flex-wrap gap-5 justify-center">
-        {data[0].saunas.slice(0,5).map((item, keys) => (
-        <Box key={keys} className="relative">
-          <CardUI accepted={false} data={item} nmd={keys}/>
-
-       
-          <CloseButton w={8} h={8} color='#3B4255' className="absolute z-10 -right-4 -top-4 p-1 shadow-sm !rounded-full !bg-[#FFFFFF]" />
-
-       
-        </Box>
-      ))}
-
+      
+           <SimpleGrid columns={{ base: 2, sm:3, md: 3, lg: 4, xl:5 }} spacing={{base: '12px', sm:'15px', lg:'20px'}}>
+           {data[0].saunas.slice(0,5).map((nm, keys) => (
+              <Box key={keys} className="relative">
+             <CardUI accepted={true} data={nm} nmd={keys} key={keys} />
+             <CloseButton w={{base: 6, md:8}} h={{base: 6, md:8}} color='#3B4255' className="absolute z-10 -right-3 md:-right-4 -top-3 md:-top-4 p-1 shadow-sm !rounded-full !bg-[#FFFFFF]" />
+             </Box>
+           ))}
+         </SimpleGrid>
+    
         </div>
     </div>
   )
